d4ca789e2d22ca4c02b5c52e50c40852452ee644
tote10 <motialemu9@gmail.com>
2025-10-19 22:15:24 +0300

    definition and add model structure

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
-# Create your models here.
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ('L', 'Low'),
+        ('M', 'Medium'),
+        ('H', 'High'),
+    ]
+    title = models.CharFiels(max_length=200)
+    description = models.TextField(blank=True,null=True)
+    priority= models.CharField(max_length=10,choices=PRIORITY_CHOICES,default='M')
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    due_date=models.DateTimeField(blank=True,null=True)
+    category=models.CharField(max_length=100,blank=True,null=True)
+    def __str__(self):
+        return self.title
