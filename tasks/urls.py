cf855a5a2c8ace4225aae13fc59393ef447bc5ce
tote10 <motialemu9@gmail.com>
2025-10-23 22:59:22 +0300

    add registration API endpoint and view for user registration

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from rest_framework import routers
 from .views import TaskViewSet
+from .views import RegisterView
 router = routers.DefaultRouter()
 router.register(r'tasks', TaskViewSet)
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('api/register/', RegisterView.as_view(), name='register'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,19 @@
 from rest_framework import viewsets
 from .models import Task
 from .serializers import TaskSerializer
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import RegisterSerializer
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user=serializer.save()
+            return Response({"username": user.username,
+            "email": user.email}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
